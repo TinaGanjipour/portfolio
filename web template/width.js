813934b0c8895d4fdeb74650bc22a701f43ef7cf
TinaGanjipour <130997275+TinaGanjipour@users.noreply.github.com>
2023-05-12 18:20:01 +0430

    First Commit

---
+++ web template/width.js
@@ -0,0 +1,11 @@
+window.addEventListener('resize',dim)
+function dim(){
+    // let width = window.innerWidth;
+    if((width>300)&&(width<=900)){
+        console.log(width);
+    }else if((width>400)&&(width<=900)){
+        console.log(width);
+    }else{
+        console.log(width);
+    }
+}
